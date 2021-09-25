d6328e48dd20c8c6cb167109a8653a541754f022
Manoel <mfmfel@gmail.com>
2021-09-25 15:15:21 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# 1 - imports - bibliotecas
+import pytest
+# 2 - class - classes
+
+# 3 - definitions - definições = métodos e funções
+
+
+def calcula_fosforos(num1,num2):
+
+    return 
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
