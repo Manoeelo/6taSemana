04e40e642bee38b3baefc7cd36b574fecdd49a69
Manoel <mfmfel@gmail.com>
2021-09-26 23:01:56 -0300

    primeiros passos

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ import pytest
 # 2 - class - classes
 
 # 3 - definitions - definições = métodos e funções
+def print_hi(name):
+    print(f'Hi, {name}')
 
+def calculo_fosforos(num,qtd):
+    return num * qtd
 
-def calcula_fosforos(num1,num2):
-
-    return 
+def teste_calculo():
+    assert calculo_fosforos(40,5) == 200
 
 
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-
+    resultado = calculo_fosforos(40,5)
+    print(f'Você comprou {resultado} palitos')
